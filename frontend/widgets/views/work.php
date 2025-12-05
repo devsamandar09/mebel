293a4194ca058b81178d9bbf346bbcee6dcbdc3d
muhammadyusufxudayberdiyev2701-crypto <muhammadyusufxudayberdiyev2701@gmail.com>
2025-12-05 16:36:23 +0500

    mebel

---
+++ frontend/widgets/views/work.php
@@ -0,0 +1,36 @@
+<section class="work-section">
+    <figure class="light-image mb-0">
+        <img src="assets/images/top-lightimage.png" alt="" class="img-fluid">
+    </figure>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="work_content">
+                    <div class="year">
+                        <span class="value counter">10</span>
+                        <sup class="mb-0 plus">+</sup>
+                    </div>
+                    <h2>Years of Successful Work in Market</h2>
+                    <div class="discover-button">
+                        <a class="discover_more text-decoration-none" href="portfolio.html">Discover More</a>
+                        <a class="image-button text-decoration-none" href="portfolio.html">
+                            <figure class="arrow mb-0">
+                                <img src="assets/images/arrow.png" alt="" class="img-fluid">
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="work_wrapper">
+                    <figure class="work-image mb-0" data-aos="fade-right">
+                        <img src="assets/images/work-image.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <figure class="work-imagebackground mb-0" data-aos="fade-up">
+                        <img src="assets/images/work-imagebackground.jpg" alt="" class="img-fluid">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
